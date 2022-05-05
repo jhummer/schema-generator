6ff1201272b427c843ccfb4254ccfdd19df98c24
Joakim Hummer <joakim.hummer@gmail.com>
2022-05-05 16:58:11 +0200

    checking for already assigned and a test

--- api/utils.py
+++ api/utils.py
@@ -7,52 +7,55 @@ def assign_task(task: Task, meeting: Meeting) -> Person:
     Either a new person qualified for the task or the one who had
     the task the longest time ago.
     """
-    # qualifiers = Person.objects.filter(qualified_for__in=[task])
-    print(f"QUALIFIERS: {task.qualifiers.all()}")
-    print(f"ASSIGNEES: {meeting.assignees.all()}")
-    latest_assigned = MeetingTaskPerson.objects.filter(task=task).order_by(
+    print(f"QUALIFIERS for {task}: {task.qualifiers.all()}")
+    # print(f"ASSIGNEES AT MEETING: {meeting.assignees.all()}")
+    latest_assigned_task = MeetingTaskPerson.objects.filter(task=task).order_by(
         "meeting__date"
     )
-    if not latest_assigned:
-        print("Picking random assignee:")
-        assignee = random.choice(task.qualifiers.all())
-        return assignee
+    if not latest_assigned_task:
+        print(f"No one has been assigned {task} before. Pick one who's available.")
+        for person in task.qualifiers.all():
+            if is_available(person, meeting):
+                return person
+        else:
+            print("No one is available")
 
-    print("pick the one who had the task the longest ago or never")
+    print(f"pick the one who had {task} the longest ago or never")
+    print(latest_assigned_task)
     never_assigned = []
     for qualifier in task.qualifiers.all():
         # for every qualifier
         assigned = False
-        for mtp in latest_assigned:
-            if mtp.person == qualifier:
-                print(f"{qualifier} was assigned {mtp.meeting}")
-                assigned = True
-                # has been assigned,
-        if not assigned:
-            print(f"{qualifier} has never been assigned")
-            never_assigned.append(qualifier)
+        print(f"[+] CHECK ASSIGNMENTS for {qualifier}")
+        for mtp in latest_assigned_task:
+            if mtp.task == task:
+                if mtp.person == qualifier:
+                    print(f"{qualifier} was assigned {task} @ {mtp.meeting}")
+                    assigned = True
+                    # has been assigned,
+                if not assigned:
+                    print(f"{qualifier} has never been assigned {task}")
+                    never_assigned.append(qualifier)
 
     for person in never_assigned:
-        rank = 0
         print(f"{person} has never been assigned {task}. Is {person} available?")
         # check if already assigned another task
-        if person in meeting.assignees.all():
-            # already assigned a task
-            # is the assigned task conflicting with the new task?
-            # TODO: handle conflicts/ranking/blacklist etc.
-            print(f"{person} is not available")
+        if is_available(person, meeting):
+            print(f"{person} is available")
+            return person
+        else:
+            # can this task be handled together with the other task?
+            # TODO: check rank/blacklist etc
             continue
-        print(f"{person} is available")
-        return person
 
     return assignee
 
 
 def is_available(person: Person, meeting: Meeting) -> bool:
     """Check if person is assigned to another task at the same meeting"""
-    for p in meeting.assignees.all():
-        if p == person:
-            print(f"{p} is assigned to: {p.task}")
-            return False
-    print(f"{person} is not assigned to any other task @ {meeting}")
+    persons_assigned = [mtp.person for mtp in meeting.assignees.all()]
+    if person in persons_assigned:
+        print(f"{person} IS assigned @ {meeting}")
+        return False
+    print(f"{person} is NOT assigned to other task @ {meeting}")
     return True
--- tests/test_assign_task.py
+++ tests/test_assign_task.py
@@ -3,32 +3,20 @@ from api.utils import assign_task
 
 
 @pytest.mark.django_db
-def test_assign_task(
+def test_assign_task_to_available_person(
     person_factory, task_factory, meeting_factory, meeting_type_factory, mtp_factory
 ):
-    print("PERSONS:")
+    """Test assigning a task to meeting with three persons qualified for three tasks
+    where two of the persons are already assigned to another task at the same event."""
+
     persons1 = person_factory.create_batch(3)
-    print(persons1)
-    persons2 = person_factory.create_batch(3)
-    print(persons2)
-    print("TASKS:")
     task1 = task_factory.create(qualifiers=persons1)
-    print(task1)
-    task2 = task_factory.create(qualifiers=persons2)
-    print(task2)
-    print("MEETING TYPE:")
-    meeting_type = meeting_type_factory.create(tasks=[task1, task2])
-    print(meeting_type)
-    print("MEETING:")
+    task2 = task_factory.create(qualifiers=persons1)
+    task3 = task_factory.create(qualifiers=persons1)
+    meeting_type = meeting_type_factory.create(tasks=[task1, task2, task3])
     meeting = meeting_factory.create(meeting_type=meeting_type)
-    print(meeting)
     mtp1 = mtp_factory(meeting=meeting, task=task1, person=persons1[0])
     mtp2 = mtp_factory(meeting=meeting, task=task2, person=persons1[1])
-    print("MTP")
-    print(mtp1)
-    print(mtp2)
 
-    assignee = assign_task(task1, meeting)
-    print(assignee)
-    print(list(task1.qualifiers.all()))
+    assignee = assign_task(task3, meeting)
     assert assignee == persons1[2]
